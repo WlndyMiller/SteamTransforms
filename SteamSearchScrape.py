9b74abe79c14b4fb310ba7e5de24cb3a9feb4c98
Windy Miller <windymiller@cock.li>
2017-07-22 19:51:42 +0000

    Refactoring to be a bit nicer

--- SteamSearchScrape.py
+++ SteamSearchScrape.py
@@ -4,10 +4,11 @@ import sys
 import dryscrape
 from bs4 import BeautifulSoup
 
-def parse_search_result(search_result):
-    soup = BeautifulSoup(search_result, 'lxml')
-    url = soup.select_one('a.searchPersonaName').get('href')
+def extract_user_profile_url(search_result):
+    soup = BeautifulSoup(str(search_result), 'lxml')
+    url =  soup.select_one('a.searchPersonaName').get('href')
     print("\nURL:" + url)
+    return url
 
     #print(soup.prettify())
 
@@ -28,28 +29,34 @@ def scrape_search(username, pages):
     # Wait for search results to load
         session.wait_for(lambda: session.at_css("div.search_row"))
         search_results.append(session.body())
-        #session.render('steam'+ str(i) +'.png')
+        # Reset session to load new page
         session.reset()
     # Take screenshot for debugging
     return search_results
 
-def parse_search(response):
+def extract_user_html(response):
     # print(searchRow)
-    usernames = []
+    users = []
     soup = BeautifulSoup(response, 'lxml')
     #for user in soup.find_all("div", class_="search_row"):
     for user in soup.select('div.search_row'):
-        print("\n\n=====================\n\n")
-        parse_search_result(str(user))
+        users.append(str(user))
+
+    return users
 
 def main():
     if len(sys.argv) != 3:
         sys.exit("Username and number of pages to search required\nSteamSearchScrape.py \"Sample User Name\" 3")
     
     responses = scrape_search(sys.argv[1], sys.argv[2])
+    users = []
     for resp in responses:
-        parse_search(resp)
-    print(len(responses))
+        users.extend(extract_user_html(resp))
+    for user in users:
+        extract_user_profile_url(user)
+
+    print(len(users))
+
 if __name__ == "__main__":
     main()
     
