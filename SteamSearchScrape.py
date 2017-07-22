73507a3ac04ce6e87812cf6c5e0f37b177965468
Windy Miller <windymiller@cock.li>
2017-07-22 19:39:34 +0000

    Added support for searching multiple pages

--- SteamSearchScrape.py
+++ SteamSearchScrape.py
@@ -4,21 +4,34 @@ import sys
 import dryscrape
 from bs4 import BeautifulSoup
 
-def parse_search_result(searchResult):
-    soup = BeautifulSoup(searchResult, 'lxml')
+def parse_search_result(search_result):
+    soup = BeautifulSoup(search_result, 'lxml')
     url = soup.select_one('a.searchPersonaName').get('href')
     print("\nURL:" + url)
 
-    print(soup.prettify())
+    #print(soup.prettify())
 
-def scrape_search(username):
+def scrape_search(username, pages):
+    """Searches for passed username for provided number of pages and returns list of DOMs (one for each page)"""
+    try:
+        pages_to_search = int(pages)
+    except ValueError:
+        pages_to_search = 1
+    search_results = []
     session = dryscrape.Session(base_url = 'https://steamcommunity.com')
-    session.visit('/search/users/#text=' + username)
+    for i in range(1, pages_to_search+1):
+        if i == 1:
+            session.visit('/search/users/#text=' + username)
+        else:
+            session.visit('/search/users/#page=' + str(i) + '&text=' + username)
+        
     # Wait for search results to load
-    session.wait_for(lambda: session.at_css("div.search_row"))
+        session.wait_for(lambda: session.at_css("div.search_row"))
+        search_results.append(session.body())
+        #session.render('steam'+ str(i) +'.png')
+        session.reset()
     # Take screenshot for debugging
-    session.render('steam.png')
-    return session.body()
+    return search_results
 
 def parse_search(response):
     # print(searchRow)
@@ -33,9 +46,10 @@ def main():
     if len(sys.argv) != 3:
         sys.exit("Username and number of pages to search required\nSteamSearchScrape.py \"Sample User Name\" 3")
     
-    response = scrape_search(sys.argv[1])
-    parse_search(response)
-
+    responses = scrape_search(sys.argv[1], sys.argv[2])
+    for resp in responses:
+        parse_search(resp)
+    print(len(responses))
 if __name__ == "__main__":
     main()
     
